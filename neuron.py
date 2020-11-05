3bd53be63704e87c2f1ea11aeab688cf5f3791ab
TomasRojas <rojaslaluz.tomas@gmail.com>
2020-11-05 03:00:16 -0300

    cambio de bias y bugfix
    se creó una neurona de bias

--- neuron.py
+++ neuron.py
@@ -1,8 +1,5 @@
-import math
-import logging
-
-# HiperVariables
-b = 1
+from math import exp
+from random import random as random
 
 
 class neuron:
@@ -11,6 +8,10 @@ class neuron:
         self.prev_Layer = prev_layer
         if prev_weights is None:
             self.prev_Weights = prev_weights
+        elif isinstance(prev_weights, int):
+            bias = []
+            for n in range(prev_weights): bias.append(random())
+            self.prev_Weights = bias
         else:
             self.prev_Weights = prev_weights.tolist()
         self.aux_Weights = []
@@ -18,16 +19,17 @@ class neuron:
         self.next_Layer = next_layer
         self.value = value
         self.delta = None
-        self.gradient = None
         self.pos = pos
 
     def activation_function(self):
-        return_value = 1 / (1 + math.exp(-self.value))
-        return return_value
+        if self.pos is None:  # caso de neurona bias
+            return 1
+        return 1 / (1 + exp(-self.value))
 
     def deriv_f(self):
-        return_value = self.activation_function() * (1 - self.activation_function())
-        return return_value
+        if self.pos is None:
+            return 1
+        return self.activation_function() * (1 - self.activation_function())
 
     def calculate_valor(self):
         if self.prev_Layer is None:
@@ -36,15 +38,20 @@ class neuron:
             ret = 0
             for i in range(len(self.prev_Layer)):
                 ret += self.prev_Weights[i] * self.prev_Layer[i].activation_function()
-            return ret + b
+            return ret
 
-    def calculate_new_weights(self, learning_rate, expected_value):
+    def update_valor(self):
+        self.value = self.calculate_valor()
+
+    def calculate_new_weights(self, learning_rate, output=None, expected_value=None):
         if self.next_Layer is None:
-            self.delta = (self.activation_function() - expected_value) * self.deriv_f()
-        elif self.prev_Layer is None:
+            self.delta = (output - expected_value) * self.deriv_f()
+        elif self.pos is None:
             summation = 0
             for n in self.next_Layer:
-                summation += n.delta * n.prev_Weights[self.pos]
+                if n.pos is None:
+                    continue
+                summation += n.delta * n.prev_Weights[-1]
             self.delta = summation * self.deriv_f()
 
             for n in self.next_Layer:
@@ -53,6 +60,8 @@ class neuron:
         else:
             summation = 0
             for n in self.next_Layer:
+                if n.pos is None:
+                    continue
                 summation += n.delta * n.prev_Weights[self.pos]
             self.delta = summation * self.deriv_f()
 
@@ -60,17 +69,35 @@ class neuron:
                 new_weight = -learning_rate * n.delta * self.activation_function()
                 self.aux_Weights.append(new_weight)
 
+    def set_next_weights(self):
+        if self.pos is None:
+            self.next_weights = self.aux_Weights.copy()
+            i = 0
+            for n in self.next_Layer:
+                self.next_weights[i] = n.prev_Weights[-1]
+                i+=1
+        elif self.next_Layer is None:
+            pass
+        else:
+            self.next_weights = self.aux_Weights.copy()
+            i = 0
+            for n in self.next_Layer:
+                self.next_weights[i] = n.prev_Weights[self.pos]
+                i += 1
+
     def update_weights(self):
         if self.prev_Layer is None:
-            self.next_weights = self.aux_Weights
+            self.next_weights += self.aux_Weights
         elif self.next_Layer is None:
             i = 0
             for n in self.prev_Layer:
                 self.prev_Weights[i] = n.next_weights[self.pos]
                 i += 1
+        elif self.pos is None:
+            self.next_weights += self.aux_Weights
         else:
             i = 0
-            self.next_weights = self.aux_Weights
+            self.next_weights += self.aux_Weights
             for n in self.prev_Layer:
                 self.prev_Weights[i] = n.next_weights[self.pos]
                 i += 1
